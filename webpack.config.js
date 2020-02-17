6a5d152ad3ae65df24067b7a7f98762626cb1a6e
Mykola Stetsyshyn <nick.s.ua@gmail.com>
2020-02-17 15:21:25 +0200

    Basic webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,36 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
+  mode: "development",
+  entry: "./src/js/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist/assets"),
+    filename: "bundle.js"
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          // options: {
-          //   presets: ['@babel/preset-env']
-          // }
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
         }
       },
-      // {
-      //   test: /\.css$/,
-      //   use: ['style-loader', 'css-loader']
-      // }
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          "css-loader",
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ]
+      }
     ]
   }
-}
+};
