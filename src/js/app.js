cf9e58db16901a9b1a09ce1377a3ee121852a47e
Mykola <nick.s.ua@gmail.com>
2020-02-29 10:54:18 +0200

    dyvokayak 0.1

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,21 @@
 import "slick-carousel";
 import jQuery from "jquery";
 
+const App = {
+  // Make navbar sticky
+  stickyMenu: () => {
+    const navbar = document.querySelector(".navbar-wrap");
+    if (window.pageYOffset >= 50) {
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }  
+}
+
 export const init = () => {
-  jQuery(function($) {
-    $(".main-slider").slick();
+  window.onscroll = function () { App.stickyMenu(); };
+  jQuery(function ($) {
+    $(".main-slider").slick();    
   });
 };
